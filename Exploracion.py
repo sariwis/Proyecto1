5f58a7213cd72805c93b66fdbeb66537212eab5e
sacero563 <s.aceros@uniandes.edu.co>
2023-09-26 16:02:51 -0500

    Histograma A_G
    Se agrega el histograma de la variable Admission Grade

--- Exploracion.py
+++ Exploracion.py
@@ -7,13 +7,13 @@ datos = 'Datos_Final.xlsx'
 df = pd.read_excel(datos)
 
 # Nombre de la columna a graficar
-columna_nombre = 'Admission grade'
+A_G = 'Admission grade'
 
 # Crear el histograma
-plt.hist(df[columna_nombre], bins=10)  # Puedes ajustar el número de bins según tus preferencias
+plt.hist(df[A_G], bins=10)  # Puedes ajustar el número de bins según tus preferencias
 plt.xlabel('Valores')
 plt.ylabel('Frecuencia')
-plt.title('Histograma de Nota ' + columna_nombre)
+plt.title('Histograma de' + A_G)
 plt.show()
 
 
