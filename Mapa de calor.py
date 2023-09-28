411797d31771f41f25ae872f0db69aae4f9ccfcc
sariwis <142942705+sariwis@users.noreply.github.com>
2023-09-27 19:39:56 -0500

    Update mapa de calor
    Equipo, realice algunos cambios a el mapa de calor. Me cuentan que opinan o si debemos tener algo más en cuenta.

--- Mapa de calor.py
+++ Mapa de calor.py
@@ -29,3 +29,30 @@ plt.title("Mapa de calor de la matriz de correlación")
 
 # Muestra el mapa de calor
 plt.show()
+
+
+datos2 = 'Factores_Finales.xlsx'
+
+#Dataframe
+df = pd.read_excel(datos2)
+
+#Cambiamos la última columna 
+df['Target'] = df['Target'].apply(lambda x: 0 if x == "Dropout" else 1)
+
+#Seleccionamos solo los que son numéricos
+df_numeric = df.select_dtypes(include=['number'])
+
+# Calculamos la matriz de correlación
+correlation_matrix = df_numeric.corr()
+
+# Configuramos los tamaños
+plt.figure(figsize=(12, 10))
+
+# Creamos el mapa de calor
+sns.heatmap(correlation_matrix, annot=True, cmap="Spectral", linewidths=.5)
+
+# Título del diagrama
+plt.title("Mapa de calor de la matriz de correlación de los factores que escogimos")
+
+# Muestra el mapa de calor
+plt.show()
