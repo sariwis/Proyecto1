9573d859e9fe4bd383ddafabb1b002bfaa80fc33
sacero563 <s.aceros@uniandes.edu.co>
2023-09-27 20:56:41 -0500

    Red Bayesiana en .py
    En el doc de Mapa de Calor, al final realicé un código que nos da la gráfica de la red bayesiana.

--- Mapa de calor.py
+++ Mapa de calor.py
@@ -56,3 +56,42 @@ plt.title("Mapa de calor de la matriz de correlación de los factores que escogi
 
 # Muestra el mapa de calor
 plt.show()
+
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Crear un grafo dirigido
+G = nx.DiGraph()
+
+# Definir las variables y sus correlaciones
+correlations = {
+    'T': ['D', 'G', 'Age'],
+    'D': ['Scol', 'Prev'],
+    'G': ['AO', 'C', 'Scol'],
+    'Age': ['AO', 'Prev', 'Scol'],
+    'Scol': ['MS'],
+    'Prev': ['C', 'AG', 'MQ', 'FQ'],
+    'AO': ['MS', 'AG'],
+    'C': ['AG'],
+    'MS': ['Scol'],
+    'MQ': ['FO', 'MO'],
+    'FQ': ['FO', 'MO'],
+}
+
+# Agregar nodos y bordes al grafo según las correlaciones
+for variable, correlaciones in correlations.items():
+    G.add_node(variable)
+    for correlacion in correlaciones:
+        G.add_edge(correlacion, variable)
+
+# Dibujar el grafo
+pos = nx.spring_layout(G, seed=42)  # Puedes ajustar el algoritmo de diseño según tus preferencias
+nx.draw(G, pos, with_labels=True, node_size=1000, node_color='lightblue', font_size=10, font_color='black', font_weight='bold', arrowsize=20)
+plt.title("Red Bayesiana basada en correlaciones")
+plt.show()
+
+
+
+
+
